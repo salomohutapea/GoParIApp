d96c20a08e177398c9593d353c073a811a29274d
hutapeasalomo@gmail.com <hutapeasalomo@gmail.com>
2019-11-16 13:39:15 +0700

    Added Fragment Home

--- app/src/main/java/com/example/gopariapp/FragmentHome.java
+++ app/src/main/java/com/example/gopariapp/FragmentHome.java
@@ -23,7 +23,7 @@ public class FragmentHome extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_home, container, false);
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
 }
