535902cbfd38f9c91a45c5abdc9cfb6385cac840
hutapeasalomo@gmail.com <hutapeasalomo@gmail.com>
2019-11-16 12:16:00 +0700

    Added Menu

--- app/src/main/java/com/example/gopariapp/MainActivity.java
+++ app/src/main/java/com/example/gopariapp/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.gopariapp;
 
+import android.support.design.bottomappbar.BottomAppBar;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        BottomAppBar bar = findViewById(R.id.bar);
+        setSupportActionBar(bar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.bottom_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.navBarHome:
+                Toast.makeText(this, "Home clicked", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
