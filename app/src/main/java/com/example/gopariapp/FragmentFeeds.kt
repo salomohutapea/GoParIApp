405793c0a0211f7480f0eb3b04b40af2a038b8ff
willyanarahardjo <willyanarahardjo@mail.ugm.ac.id>
2019-11-16 20:28:14 +0700

    new

--- app/src/main/java/com/example/gopariapp/FragmentFeeds.kt
+++ app/src/main/java/com/example/gopariapp/FragmentFeeds.kt
@@ -1,10 +1,12 @@
 package com.example.gopariapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_feeds.*
 
 
 /**
@@ -13,10 +15,18 @@ import android.view.ViewGroup
 class FragmentFeeds : Fragment() {
 
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feeds, container, false)
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.fragment_home, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fabFeeds.setOnClickListener {
+            val matematikaIntent = Intent(requireActivity(), NewFeedsActivity::class.java)
+            requireActivity().startActivity(matematikaIntent)
+        }
     }
 
 }// Required empty public constructor
